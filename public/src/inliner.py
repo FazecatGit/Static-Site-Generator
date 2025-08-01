e1179b13874142fab6f4435173a521aa487b2ec1
Fazecat <peterisnot99@gmail.com>
2025-08-01 22:04:11 +0100

    blocks :D

--- public/src/inliner.py
+++ public/src/inliner.py
@@ -66,3 +66,14 @@ def split_nodes_links(old_nodes):
             elif i % 3 == 2:
                 new_nodes[-1].url = sections[i]
     return new_nodes
+
+def text_to_textnode(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_images(nodes)
+    nodes = split_nodes_links(nodes)
+    
+    return nodes
--- public/src/textnode.py
+++ public/src/textnode.py
@@ -23,20 +23,20 @@ class TextNode:
             self.url == other.url
         )
 
-    def text_node_to_html_node(self):
-        if self.text_type == TextType.TEXT:
-            return LeafNode(tag=None, value=self.text)
-        if self.text_type == TextType.LINK:
-            return LeafNode(tag="a", value=self.text, props={"href": self.url})
-        if self.text_type == TextType.BOLD:
-            return LeafNode(tag="b", value=self.text)
-        if self.text_type == TextType.ITALIC:
-            return LeafNode(tag="i", value=self.text)
-        if self.text_type == TextType.CODE:
-            return LeafNode(tag="code", value=self.text)
-        if self.text_type == TextType.IMAGE:
-            return LeafNode(tag="img", value= "", props={"src": self.url, "alt": self.text})
-    
+
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+def text_node_to_html_node(self):
+    if self.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=self.text)
+    if self.text_type == TextType.LINK:
+        return LeafNode(tag="a", value=self.text, props={"href": self.url})
+    if self.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=self.text)
+    if self.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=self.text)
+    if self.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=self.text)
+    if self.text_type == TextType.IMAGE:
+        return LeafNode(tag="img", value= "", props={"src": self.url, "alt": self.text})
