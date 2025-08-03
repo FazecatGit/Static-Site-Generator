cbab80fbad322255e28e132312ff1242aace274e
Fazecat <peterisnot99@gmail.com>
2025-08-03 22:50:24 +0100

    change base path

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     delete_content_directory("docs")
     recursive_copy("static", "docs")
-    generate_page_recursive("content", "template.html", "docs", basepath="/")
-    generate_page("content/index.md", "template.html", "docs/index.html", basepath)
+    generate_page_recursive("content", "template.html", "docs", basepath=basepath)
+    generate_page("content/index.md", "template.html", "docs/index.html", basepath=basepath)
 
 if __name__ == "__main__":
     main()
